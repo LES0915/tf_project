3c205f0576aa013c146b03bfaa6d73e9b94cf030
LES0915 <silver_lives@naver.com>
2026-07-27 14:50:08 +0900

    Feat: tfvars 사용법 (가장많이 사용됨)

--- tf-step2/syntax.tf
+++ tf-step2/syntax.tf
@@ -1,27 +1,27 @@
 # 1. 변수
 # 변수 선언 및 값 세팅
 variable "name" {
-    # 문자열 타입
-    default = "테라폼"
+  # 문자열 타입
+  default = "테라폼"
 }
 variable "age" {
-    # 수치 타입
-    default = 100
+  # 수치 타입
+  default = 100
 }
 variable "is_mz" {
-    # 불린 타입
-    default = true
+  # 불린 타입
+  default = true
 }
 variable "books" {
-    # 리스트 타입
-    default = ["1","2"]
+  # 리스트 타입
+  default = ["1", "2"]
 }
 variable "stations" {
-    # map 타입
-    default = {
-        A = "영등포역",
-        B = 100
-    }
+  # map 타입
+  default = {
+    A = "영등포역",
+    B = 100
+  }
 }
 
 # 출력
@@ -45,13 +45,13 @@ output "stations" {
 # tfvars 테스트
 ##########################################################################
 variable "environment" {
-  type = string # 명시적 타입 지정
-  default = "dev"
+  type        = string # 명시적 타입 지정
+  default     = "dev"
   description = "배포 환경 (dev -> stage -> prod)"
 }
 variable "instance_count" {
-  type = number
-  default = 1
+  type        = number
+  default     = 1
   description = "생성할 서버 개수"
 }
 
