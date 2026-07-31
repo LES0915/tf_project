1c34dd059d5ec4ae3f51000feebc0c3ddea93496
LES0915 <silver_lives@naver.com>
2026-07-31 10:51:38 +0900

    Feat: locals.tf 구성 완료

--- tf-step5/locals.tf
+++ tf-step5/locals.tf
@@ -8,13 +8,28 @@ locals {
     project = "DE-AI-13-IaC-3tier-V1"
     # 리소스에 적용된 공용 태그 -> 커스텀 구성 태그들을 리소스에 공통 배치하기 위함
     common_tags = {
-        Project = locals.project
+        Project = local.project
         Environment = var.environment
         ManageBy = "Terraform"
     }
-    # 서울 리전 2개 가용영역(a, c) 사용
-    # ALB
-    # WEB/WAS ASG
-    # RDS
-
+    # 도쿄 리전 2개 가용영역(a, c) 사용 (본인 속한 리전 이름으로 변경)
+    azs = {
+        a = "ap-northeast-1a"
+        c = "ap-northeast-1c"
+    }
+    # ALB -> 2개 가용영역(a, c)에 서브넷 각각 1개(퍼블릭) -> cidr 설정
+    public_subnets = {
+        a = "10.0.1.0/24"
+        c = "10.0.2.0/24"
+    }
+    # WEB/WAS ASG -> cidr
+    app_subnets = {
+        a = "10.0.11.0/24"
+        c = "10.0.12.0/24"
+    }
+    # RDS -> cidr
+    db_subnets = {
+        a = "10.0.21.0/24"
+        c = "10.0.22.0/24"
+    }
 }
