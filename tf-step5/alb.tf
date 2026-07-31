df5d7d5a39ed2b70a5ec707181c8c0d8e13adf36
LES0915 <silver_lives@naver.com>
2026-07-31 14:32:13 +0900

    Feat: ALB의 타겟 Ec2 점검 헬스체크 구성 코멘트

--- tf-step5/alb.tf
+++ tf-step5/alb.tf
@@ -7,26 +7,28 @@ resource "aws_lb" "public" {
   internal           = false
   load_balancer_type = "application"
   # 보안그룹 - 퍼블릭 ALB
-  security_groups    = [aws_security_group.public_alb.id]
+  security_groups = [aws_security_group.public_alb.id]
   # 퍼블릭 서브넷 2개 (가용영역별 a, c) 각각 id를 추출(리스트 컴프리헨션 방식 문법) 반영
-  subnets            = [for subnet in aws_subnet.public : subnet.id]
-  tags = { Name = "${local.project}-PUBLIC-ALB" }
+  subnets = [for subnet in aws_subnet.public : subnet.id]
+  tags    = { Name = "${local.project}-PUBLIC-ALB" }
 }
+# ALB가 주기적으로 대상 EC2의 상태를 체크하는 설정(헬스체크 등)
 resource "aws_lb_target_group" "web" {
   name        = "${local.project}-web-tg"
   port        = 80
   protocol    = "HTTP"
   target_type = "instance"
   vpc_id      = aws_vpc.main.id
+  # 주기적으로 대상 EC2 점검 -> 요청 -> 응답에 대한 규칙 설정
   health_check {
-    enabled             = true
-    path                = "/health"
-    protocol            = "HTTP"
-    matcher             = "200"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
+    enabled             = true      # 헬스 체크 사용 여부
+    path                = "/health" # 상태 체크하는 URL 값(경로)
+    protocol            = "HTTP"    # 통신 방식
+    matcher             = "200"     # 정상 응답 코드 값
+    interval            = 30        # 헬스 체크 간격 (30초)
+    timeout             = 5         # 응답 대기 시간 (초)
+    healthy_threshold   = 2         # 연속 몇번 (2번) 성공해야 정상 상태로 인지(상태변경)
+    unhealthy_threshold = 2         # 연속 몇번 (2번) 실패해야 비정상 상태로 인지(상태변경)
   }
   tags = { Name = "${local.project}-WEB-TG" }
 }
@@ -49,9 +51,10 @@ resource "aws_lb" "internal" {
   load_balancer_type = "application"
   security_groups    = [aws_security_group.internal_alb.id]
   # APP 프라이빗 서브넷 2개 (가용영역별 a, c) 각각 id를 추출(리스트 컴프리헨션 방식 문법) 반영
-  subnets            = [for subnet in aws_subnet.app : subnet.id]
-  tags = { Name = "${local.project}-INTERNAL-ALB" }
+  subnets = [for subnet in aws_subnet.app : subnet.id]
+  tags    = { Name = "${local.project}-INTERNAL-ALB" }
 }
+# was 직접 점검 => 8000번 점속, 모두 상위 설정값과 동일
 resource "aws_lb_target_group" "was" {
   name        = "${local.project}-was-tg"
   port        = 8000
--- tf-step5/asg.tf
+++ tf-step5/asg.tf
@@ -1,10 +1,10 @@
 resource "aws_autoscaling_group" "web" {
-  name = "${local.project}-WEB-ASG"
-  min_size         = 2
-  desired_capacity = var.web_desired_capacity
-  max_size         = 4
-  vpc_zone_identifier = [for subnet in aws_subnet.app : subnet.id]
-  target_group_arns   = [aws_lb_target_group.web.arn]
+  name                      = "${local.project}-WEB-ASG"
+  min_size                  = 2
+  desired_capacity          = var.web_desired_capacity
+  max_size                  = 4
+  vpc_zone_identifier       = [for subnet in aws_subnet.app : subnet.id]
+  target_group_arns         = [aws_lb_target_group.web.arn]
   health_check_type         = "ELB"
   health_check_grace_period = 180
   launch_template {
@@ -30,12 +30,12 @@ resource "aws_autoscaling_group" "web" {
   }
 }
 resource "aws_autoscaling_group" "was" {
-  name = "${local.project}-WAS-ASG"
-  min_size         = 2
-  desired_capacity = var.was_desired_capacity
-  max_size         = 4
-  vpc_zone_identifier = [for subnet in aws_subnet.app : subnet.id]
-  target_group_arns   = [aws_lb_target_group.was.arn]
+  name                      = "${local.project}-WAS-ASG"
+  min_size                  = 2
+  desired_capacity          = var.was_desired_capacity
+  max_size                  = 4
+  vpc_zone_identifier       = [for subnet in aws_subnet.app : subnet.id]
+  target_group_arns         = [aws_lb_target_group.was.arn]
   health_check_type         = "ELB"
   health_check_grace_period = 240
   launch_template {
