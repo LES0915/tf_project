ddb98a5504bd6d514f13a724ee249687f9cefff6
LES0915 <silver_lives@naver.com>
2026-07-31 11:26:43 +0900

    Feat: vpc 기본 골격

---
+++ tf-step5/vpc.tf
@@ -0,0 +1,17 @@
+# VPC
+
+# Internet Gateway
+
+# Public Subnets - Public ALB, NAT Gateway
+
+# Private Application Subnets - Web, Was, Internal ALB
+
+# Private DB Subnets - RDS
+
+# Public Route Table/association 
+
+# Nat Gateway - EIP
+
+# Private App Route Table/association - Web, Was
+
+# Private Db Route Table/association - RDS
