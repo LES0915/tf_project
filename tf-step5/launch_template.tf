4c775bb8e6506a7b17ba4121711f1d05fac4ce6c
LES0915 <silver_lives@naver.com>
2026-07-31 16:41:05 +0900

    Fix: encode 수정

--- tf-step5/launch_template.tf
+++ tf-step5/launch_template.tf
@@ -5,14 +5,14 @@ resource "aws_launch_template" "web" {
   name_prefix   = "${local.project}-WEB-" # 증감이 수시로 발생해도 중복 x
   image_id      = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
-  # SSM 접속을 위해 프로파일 설정 -> iam.tf 구성
+  # SSM 접속을 위해 프로파링 설정 -> iam.tf 구성
   iam_instance_profile {
     name = aws_iam_instance_profile.ec2_ssm.name
   }
   # 보안 그룹 지정
   vpc_security_group_ids = [aws_security_group.web.id]
-  # 서버 구성 후 초기 작업 -> 쉘 스크립트를 읽어서 => base64 인코딩 처리 => 실행되게 구성
-  user_data = base64decode(templatefile("${path.module}/userdata-web.sh.tftpl", {
+  # 서버 구성후 초기 작업 -> 쉘스크립트를 읽어서 => base64 인코딩처리 => 실행되게 구성
+  user_data = base64encode(templatefile("${path.module}/userdata-web.sh.tftpl", {
     # WEB -> proxy -> Internal ALB (이 리소스의 IP 혹은 dns 알아야 전달)
     # 해당 값을 가져가서 nginx conf 파일을 완성함
     internal_alb_dns = aws_lb.internal.dns_name
@@ -25,7 +25,7 @@ resource "aws_launch_template" "web" {
       Tier = "web"
     })
   }
-  # 삭제 전 생성 -> ec2 수 유지에 대한 기준
+  # 삭제전 생성 -> ec2 수 유지에 대한 기준
   lifecycle {
     create_before_destroy = true
   }
@@ -43,7 +43,7 @@ resource "aws_launch_template" "was" {
     name = aws_iam_instance_profile.ec2_ssm.name
   }
   vpc_security_group_ids = [aws_security_group.was.id]
-  user_data = base64decode(templatefile("${path.module}/userdata-was.sh.tftpl", {
+  user_data = base64encode(templatefile("${path.module}/userdata-was.sh.tftpl", {
     # rds 세팅값 설정
     db_host = aws_db_instance.mysql.address
     db_port = aws_db_instance.mysql.port
