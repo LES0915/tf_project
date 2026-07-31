d25add8622507bed88384d48febc033750c45b61
LES0915 <silver_lives@naver.com>
2026-07-31 10:37:04 +0900

    Feat: 전역변수 설정. tf 구성 전역에서 사용

---
+++ tf-step5/variables.tf
@@ -0,0 +1,44 @@
+#######################################
+# tf 전체에서 사용할 변수 8개(컨셉) 정의
+#######################################
+
+variable "region" {
+  description = "AWS 리전"
+  type        = string
+  default     = "ap-northeast-1"
+}
+variable "environment" {
+  description = "구동 환경"
+  type        = string
+  default     = "dev"
+}
+variable "instance_type" {
+  description = "WEB/WAS EC2 인스턴스 유형"
+  type        = string
+  default     = "t3.micro"
+}
+variable "web_desired_capacity" { # 테라폼이 구축할 때 기본 2개 생성
+  description = "WEB ASG 기본 인스턴스 수" 
+  type        = number
+  default     = 2
+}
+variable "was_desired_capacity" {
+  description = "WAS ASG 기본 인스턴스 수"
+  type        = number
+  default     = 2
+}
+variable "db_instance_class" {
+  description = "DB 인스턴스 클래스"
+  type        = string
+  default     = "db.t3.micro"
+}
+variable "db_name" {
+  description = "초기 생성 데이터베이스 이름"
+  type        = string
+  default     = "appdb"
+}
+variable "user_name" {
+  description = "RDS 관리자 이름"
+  type        = string
+  default     = "adminuser"
+}
