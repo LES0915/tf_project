a912a9628c58b3cc64b20829a1f483010b9e5028
LES0915 <silver_lives@naver.com>
2026-07-31 13:46:07 +0900

    Fix: 오류 수정

--- tf-step5/vpc.tf
+++ tf-step5/vpc.tf
@@ -124,7 +124,7 @@ resource "aws_route_table" "app" {
   }
 }
 
-resource "aws_route_table_association" "private" {
+resource "aws_route_table_association" "app" {
   for_each       = aws_subnet.app
   subnet_id      = each.value.id
   route_table_id = aws_route_table.app[each.key].id
@@ -139,7 +139,7 @@ resource "aws_route_table" "db" {
   }
 }
 
-resource "aws_route_table_association" "private" {
+resource "aws_route_table_association" "db" {
   for_each       = aws_subnet.db
   subnet_id      = each.value.id
   route_table_id = aws_route_table.db.id
